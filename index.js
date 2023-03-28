c862abd15cedf191ef8acc71c7e9d1df2373e504
tycoge <tyler31763@gmail.com>
2023-03-28 14:23:04 -0400

    started project, began loop for grid

---
+++ index.js
@@ -0,0 +1,10 @@
+// 16 x 16 grid divs, place inside container div(place that inside the html)
+// divs appear as grid with flexbox(careful with borders and margins)
+//link style sheet,
+const container = document.querySelector('.container')
+for(let i = 0;i <= 16;i++){
+    for(let i = 0;i <= 16;i++){
+        let cell = document.createElement('div')
+        .container.append
+    }
+}
