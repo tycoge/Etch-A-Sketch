33d81f4bede0da2b890ec36d9fe64ca0b1b06f87
tycoge <tyler31763@gmail.com>
2023-05-01 15:10:41 -0400

    testing

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // 16 x 16 grid divs, place inside container div(place that inside the html)
 // divs appear as grid with flexbox(careful with borders and margins)
-//link style sheet,
-const container = document.querySelector('.container')
-for(let i = 0;i <= 16;i++){
-    for(let i = 0;i <= 16;i++){
-        let cell = document.createElement('div')
-        cell.textContent = 'f'
-        container.appendChild(cell)
+//link style sheet, hover change color eventlistener,
+const container = document.querySelector('.container');
+const button = document.querySelector('.button');
+let gridSize = 16
+button.addEventListener('click',()=>{grid(
+gridSize = prompt('Enter grid size','65'))
 
+})
+function grid(gridSize){
+    for(let i = 0;i < gridSize;i++){
+        let row = document.createElement('div')
+        row.classList.add('row')
+        row.textContent = ``
+        row.style.cssText = 'display:flex;flex-basis:100%'
+        container.appendChild(row)
+        for(let i = 0;i < gridSize;i++){
+            let cell = document.createElement('div');
+            cell.classList.add('grid')
+            cell.textContent = ``
+            cell.style.cssText = 'background-color:white;flex:1;aspect-ratio:1/1'
+            row.appendChild(cell);
+            cell.addEventListener('mouseover',function (e){
+                e.target.style.background = 'black'
+            })
+        }
     }
 }
+grid(gridSize)
