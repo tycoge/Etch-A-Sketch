b4ddb05a71d9a1dbb77337bc6ab983b5b89228b8
tycoge <tyler31763@gmail.com>
2023-05-03 13:44:24 -0400

    final

--- index.js
+++ index.js
@@ -4,21 +4,19 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.button');
 let gridSize = 16
+button.addEventListener('click',clearGrid)
 button.addEventListener('click',()=>{grid(
-gridSize = prompt('Enter grid size','65'))
-
+    gridSize = prompt('Enter grid size','65'))
 })
 function grid(gridSize){
     for(let i = 0;i < gridSize;i++){
         let row = document.createElement('div')
         row.classList.add('row')
-        row.textContent = ``
         row.style.cssText = 'display:flex;flex-basis:100%'
         container.appendChild(row)
         for(let i = 0;i < gridSize;i++){
             let cell = document.createElement('div');
             cell.classList.add('grid')
-            cell.textContent = ``
             cell.style.cssText = 'background-color:white;flex:1;aspect-ratio:1/1'
             row.appendChild(cell);
             cell.addEventListener('mouseover',function (e){
@@ -27,4 +25,8 @@ function grid(gridSize){
         }
     }
 }
+function clearGrid(){
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild)}
+    }
 grid(gridSize)
